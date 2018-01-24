d1d3a7ac491b99ce6fb63e277111c740b218c5e2
Aareon Sullivan <askully13@gmail.com>
2018-01-24 11:24:01 -0800

    Update decrypt.py to Py3
    Save Py2 compatibility

--- tools/decrypt.py
+++ tools/decrypt.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-from Cryptodome.Cipher import AES
-import hashlib
+from __future__ import print_function
+
 from getpass import getpass
+import hashlib
+import sys
+from Cryptodome.Cipher import AES
+
+# Python 2 & 3 compatibility
+_PY3 = sys.version_info[0] > 2
+if _PY3:
+  raw_input = input
 
 encrypted = raw_input("Cipher: ")
 passphrase = getpass("Enter passphrase: ")
@@ -18,4 +26,4 @@ cipher = AES.new(hashedpass, AES.MODE_EAX, nonce)
 private_key = cipher.decrypt_and_verify(ciphertext, tag)
 
 print("Decrypted private key: ")
-print private_key
+print(private_key)
