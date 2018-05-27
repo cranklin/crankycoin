a7ed526b3adee683a18837735686a8fcd2a57d70
cranklin <cranklin@gmail.com>
2018-05-26 17:14:25 -0700

    Add validator service happy path tests and add trial pubsub service

--- crankycoin/services/pubsub.py
+++ crankycoin/services/pubsub.py
@@ -1,22 +1,75 @@
 import zmq
-import random
-import sys
 import time
 
 from crankycoin import config, logger
 
+"""
+Experimental.  
+"""
 class PubSub(object):
 
+    PUBSUB_PORT = config['network']['full_node_port']
+
     @classmethod
-    def start_pubsub(cls):
+    def sync_with_subscriber(cls, bind_to):
+        # use bind socket + 1
+        sync_with = ':'.join(bind_to.split(':')[:-1] +
+                             [str(int(bind_to.split(':')[-1]) + 1)])
+        ctx = zmq.Context.instance()
+        s = ctx.socket(zmq.REP)
+        s.bind(sync_with)
+        print("Waiting for subscriber to connect...")
+        s.recv()
+        print("   Done.")
+        s.send('GO')
+
+    @classmethod
+    def sync_with_publisher(cls, connect_to):
+        # use connect socket + 1
+        sync_with = ':'.join(connect_to.split(':')[:-1] +
+                             [str(int(connect_to.split(':')[-1]) + 1)]
+                             )
+        ctx = zmq.Context.instance()
+        s = ctx.socket(zmq.REQ)
+        s.connect(sync_with)
+        s.send('READY')
+        s.recv()
+
+    @classmethod
+    def start_publisher(cls):
         try:
-            port = "5556"
-            if len(sys.argv) > 1:
-                port =  sys.argv[1]
-                int(port)
+            bind_to = "tcp://*:{}".format(cls.PUBSUB_PORT)
+            ctx = zmq.Context()
+            s = ctx.socket(zmq.PUB)
+            s.bind(bind_to)
 
-            context = zmq.Context()
-            server_socket = context.socket(zmq.PUB)
-            server_socket.bind("tcp://*:%s" % port)
+            #cls.sync_with_subscriber(bind_to)
+        except Exception as e:
+            logger.error("could not start publishing server: %s", e.message)
+            raise
 
-            client_socket = context.socket(zmq.SUB)
+        print("Publishing...")
+        while True:
+            a = time.clock()
+            s.send_string(str(a))
+
+    @classmethod
+    def start_subscriber(cls):
+        try:
+            connect_to = "tcp://localhost:{}".format(cls.PUBSUB_PORT)
+            array_count = 10
+            ctx = zmq.Context()
+            s = ctx.socket(zmq.SUB)
+            s.connect(connect_to)
+            s.setsockopt(zmq.SUBSCRIBE,'')
+
+            #cls.sync_with_publisher(connect_to)
+        except Exception as e:
+            logger.error("could not start subscribe: %s", e.message)
+            raise
+
+        print("Listening...")
+        while True:
+            a = s.recv_string()
+            print(a)
+
--- crankycoin/services/test/test_validator.py
+++ crankycoin/services/test/test_validator.py
@@ -1,6 +1,7 @@
-from unittest import TestCase
-from mock import patch, Mock
+from unittest import TestCase, skip
+from mock import patch, Mock, MagicMock
 
+from crankycoin import config
 from crankycoin.models.enums import TransactionType
 from crankycoin.repository.blockchain import Blockchain
 from crankycoin.repository.mempool import Mempool
@@ -13,10 +14,12 @@ class TestValidator(TestCase):
 
     def setUp(self):
         self.mock_blockchain = Mock(Blockchain)
+        self.mock_mempool = Mock(Mempool)
         with patch.object(Blockchain, "__init__", return_value=None) as patched_blockchain, \
             patch.object(Mempool, "__init__", return_value=None) as patched_mempool:
             self.subject = Validator()
         self.subject.blockchain = self.mock_blockchain
+        self.subject.mempool = self.mock_mempool
 
     def test_check_hash_and_hash_pattern_When_valid_Returns(self):
         self.mock_blockchain.calculate_hash_difficulty.return_value = 4
@@ -53,3 +56,57 @@ class TestValidator(TestCase):
 
         response = self.subject.check_block_reward(mock_block)
         self.assertTrue(response)
+
+    def test_validate_block_header(self):
+        self.mock_blockchain.get_block_header_by_hash.side_effect = [None, (Mock(), 0, 15)]
+        self.mock_blockchain.calculate_hash_difficulty.return_value = 4
+        mock_block_header = Mock(BlockHeader)
+        mock_block_header.version = config['network']['version']
+        mock_block_header.merkle_root = "0123456789ABCDEF"
+        mock_block_header.hash_difficulty = 4
+        mock_block_header.previous_hash = "0000123412341234"
+
+        with patch('crankycoin.services.validator.Validator.calculate_merkle_root', return_value="0123456789ABCDEF") as patched_calculate_merkle_root:
+            response = self.subject.validate_block_header(mock_block_header, [])
+
+        self.assertTrue(response)
+
+    def test_validate_block(self):
+        mock_block_header = Mock(BlockHeader)
+        mock_block_header.merkle_root = "0123456789ABCDEF"
+        mock_block = Mock(Block)
+        mock_block.block_header = mock_block_header
+
+        with patch('crankycoin.services.validator.Validator.check_block_reward', return_value=True) as patched_check_block_reward:
+            response = self.subject.validate_block(mock_block, "0123456789ABCDEF")
+
+        self.assertTrue(response)
+
+    def test_validate_block_transactions_inv(self):
+        mock_transaction = Mock(Transaction)
+        self.mock_blockchain.find_duplicate_transactions.return_value = False
+        self.mock_mempool.get_unconfirmed_transaction.side_effect = [None, mock_transaction]
+
+        response = self.subject.validate_block_transactions_inv(['tx_hash_one', 'tx_hash_two'])
+
+        self.assertIn('tx_hash_one', response[1])
+        self.assertIn(mock_transaction, response[0])
+
+    def test_validate_transaction(self):
+        self.mock_blockchain.find_duplicate_transactions.return_value = False
+        self.mock_blockchain.get_balance.return_value = 100
+        mock_transaction = Mock(Transaction)
+        mock_transaction.tx_hash = "0123456789ABCDEF"
+        mock_transaction.verify.return_value = True
+        mock_transaction.source = "03dd1e00000000000000000"
+        mock_transaction.amount = 95
+        mock_transaction.fee = 5
+
+        response = self.subject.validate_transaction(mock_transaction)
+
+        self.assertTrue(response)
+
+    @skip
+    def test_calculate_merkle_root(self):
+        raise NotImplementedError
+
