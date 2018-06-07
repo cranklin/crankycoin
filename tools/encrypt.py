e7724935ce4d07cd28a231c5e849be2a123a5502
Aareon Sullivan <askully13@gmail.com>
2018-06-07 15:16:30 -0700

    Update for Python 3 encoding
    Fixes https://github.com/cranklin/crankycoin/issues/12

--- tools/encrypt.py
+++ tools/encrypt.py
@@ -10,9 +10,10 @@ from Cryptodome.Cipher import AES
 _PY3 = sys.version_info[0] > 2
 if _PY3:
     raw_input = input
+    import codecs
 
-passphrase = getpass("Choose a passphrase: ")
-verifypass = getpass("Re-enter passphrase: ")
+passphrase = getpass("Choose a passphrase: ").encode('utf-8')
+verifypass = getpass("Re-enter passphrase: ").encode('utf-8')
 
 if passphrase != verifypass:
     print("Passphrases do not match")
@@ -21,9 +22,12 @@ if passphrase != verifypass:
 secret = raw_input("Secret: ")
 hashedpass = hashlib.sha256(passphrase).digest()
 cipher = AES.new(hashedpass, AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(secret)
+ciphertext, tag = cipher.encrypt_and_digest(secret.encode('utf-8'))
 
 combined = "{}{}{}".format(cipher.nonce, tag, ciphertext)
 
 print("Encrypted private key: ")
-print(combined.encode('hex'))
+if not _PY3:
+    print(combined.encode('hex')
+else:
+    codecs.encode(combined.encode('utf-8'), 'hex'))
