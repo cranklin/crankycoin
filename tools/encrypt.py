79aa56b00ff4fe4599e3067506151a39889a885b
Aareon Sullivan <askully13@gmail.com>
2018-06-10 09:03:00 -0700

    Update encrypt.py

--- tools/encrypt.py
+++ tools/encrypt.py
@@ -2,32 +2,30 @@
 
 from __future__ import print_function
 
+import codecs
 import hashlib
 from getpass import getpass
 import sys
+
 from Cryptodome.Cipher import AES
 
 _PY3 = sys.version_info[0] > 2
-if _PY3:
-    raw_input = input
-    import codecs
+if not _PY3:
+    input = raw_input
 
-passphrase = getpass("Choose a passphrase: ").encode('utf-8')
-verifypass = getpass("Re-enter passphrase: ").encode('utf-8')
+passphrase = getpass("Choose a passphrase: ")
+verifypass = getpass("Re-enter passphrase: ")
 
 if passphrase != verifypass:
     print("Passphrases do not match")
     sys.exit(1)
 
 secret = raw_input("Secret: ")
-hashedpass = hashlib.sha256(passphrase).digest()
+hashedpass = hashlib.sha256(passphrase.encode('utf-8').digest()
 cipher = AES.new(hashedpass, AES.MODE_EAX)
 ciphertext, tag = cipher.encrypt_and_digest(secret.encode('utf-8'))
 
 combined = "{}{}{}".format(cipher.nonce, tag, ciphertext)
 
 print("Encrypted private key: ")
-if not _PY3:
-    print(combined.encode('hex')
-else:
-    codecs.encode(combined.encode('utf-8'), 'hex'))
+print(codecs.encode(combined.encode('utf-8'), 'hex').decode())
