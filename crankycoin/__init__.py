c7bcf96b490966e7d81c4f4d177714a58cac3338
cranklin <cranklin@gmail.com>
2021-03-02 18:27:17 -0800

    Update __init__.py to use PyYAML in a safer manner and avoid warning messages

--- crankycoin/__init__.py
+++ crankycoin/__init__.py
@@ -2,7 +2,7 @@ import logging
 import yaml
 
 with open("config/config.yaml", 'r') as ymlfile:
-    config = yaml.load(ymlfile)
+    config = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
