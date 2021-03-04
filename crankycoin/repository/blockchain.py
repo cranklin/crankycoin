41c7ac6226842fb867b5eaa554c3604e998507c2
cranklin <cranklin@gmail.com>
2021-03-04 09:37:07 -0800

    Fix python3 compatible integer division

--- crankycoin/repository/blockchain.py
+++ crankycoin/repository/blockchain.py
@@ -233,7 +233,7 @@ class Blockchain(object):
     def get_reward(self, height):
         precision = pow(10, self.SIGNIFICANT_DIGITS)
         reward = self.INITIAL_COINS_PER_BLOCK
-        for i in range(1, ((height / self.HALVING_FREQUENCY) + 1)):
+        for i in range(1, ((height // self.HALVING_FREQUENCY) + 1)):
             reward = floor((reward / 2.0) * precision) / precision
         return reward
 
